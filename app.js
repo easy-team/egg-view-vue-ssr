b34113b25266ea0e996a664d42929bd14abe3b1b
hubcarl <hubcarl@126.com>
2017-04-24 20:20:55 +0800

    feat:support renderbunder option config

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const Resource = require('./lib/resource');
 module.exports = app => {
   app.vue.resource = new Resource(app);
-  if (app.config.vuessr.layout) {
-    app.config.vuessr.template = fs.readFileSync(app.config.vuessr.layout, 'utf8');
+  const layout = app.config.vuessr.layout;
+  if (layout && fs.existsSync(layout)) {
+    app.vue.resource.template = fs.readFileSync(layout, 'utf8');
+  } else if (app.vue.renderOptions && app.vue.renderOptions.template) {
+    app.vue.resource.template = app.vue.renderOptions.template;
   }
 };
--- app/extend/context.js
+++ app/extend/context.js
@@ -9,14 +9,15 @@ module.exports = {
    * @param {Object} [locals] template data
    * @param {Object} options custom params
    */
-  * render(name, locals, options) {
+  * render(name, locals, options = {}) {
     locals = Object.assign({}, this.app.locals, this.locals, locals);
     const root = this.app.config.view.root[0];
     const filepath = path.join(root, name);
     const code = yield this.app.vue.readFile(filepath);
+    const template = options.renderOptions && options.renderOptions.template || this.app.vue.resource.template;
     const html = yield this.app.vue.renderCode(code, locals, options).catch(err => {
       this.app.logger.error('[%s] server render bundle error, try client render, the server render error', name, err);
-      return this.renderString(this.app.config.vuessr.template, locals);
+      return this.renderString(template, locals);
     });
     this.body = this.app.vue.resource.inject(name, html, locals, options);
   },
