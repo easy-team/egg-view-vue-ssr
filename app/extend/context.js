80f9525f7f2adedfabfb3ea27d8152bfb7b8b697
hubcarl <hubcarl@126.com>
2017-08-28 12:03:48 +0800

    fix:node async and await

--- app/extend/context.js
+++ app/extend/context.js
@@ -8,30 +8,28 @@ module.exports = {
    * @param {String} name filename
    * @param {Object} [locals] template data
    * @param {Object} options custom params
+   * @return {Object} Promise
    */
-  * render(name, locals, options = {}) {
+  render(name, locals, options = {}) {
     locals = Object.assign({}, this.app.locals, this.locals, locals);
     const config = this.app.config.vuessr;
     const template = options.renderOptions && options.renderOptions.template || this.app.vue.resource.template;
-    const context = { state: locals };
     const filepath = path.join(this.app.config.view.root[0], name);
-    let html = '';
-    if (options.renderClient) {
-      html = yield this.renderString(template, context.state);
-    } else {
-      html = yield this.app.vue.renderBundle(filepath, context, options).catch(err => {
-        if (config.fallbackToClient) {
-          this.app.logger.error('[%s] server render bundle error, try client render, the server render error', name, err);
-          return this.renderString(template, context.state);
-        }
-        throw err;
-      });
-    }
-    this.body = this.app.vue.resource.inject(html, context, name, config, options);
+    const context = { state: locals };
+    const promise = options.renderClient ? this.renderString(template, context.state) : this.app.vue.renderBundle(filepath, context, options).catch(err => {
+      if (config.fallbackToClient) {
+        this.app.logger.error('[%s] server render bundle error, try client render, the server render error', name, err);
+        return this.renderString(template, context.state);
+      }
+      throw err;
+    });
+    return promise.then(html => {
+      this.body = this.app.vue.resource.inject(html, context, name, config, options);
+    });
   },
 
-  * renderClient(name, locals, options = {}) {
+  renderClient(name, locals, options = {}) {
     options.renderClient = true;
-    yield this.render(name, locals, options);
+    return this.render(name, locals, options);
   },
 };
