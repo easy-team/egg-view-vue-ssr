a441910cb59a3d366f39e1b4623e427c496c4c32
hubcarl <hubcarl@126.com>
2017-11-16 15:12:59 +0800

    refacor: not dependence egg-view-vue;

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict';
 const fs = require('fs');
-const Resource = require('./lib/resource');
 module.exports = app => {
-  app.vue.resource = new Resource(app);
-  const layout = app.config.vuessr.layout;
-  if (layout && fs.existsSync(layout)) {
-    app.vue.resource.template = fs.readFileSync(layout, 'utf8');
-  } else if (app.vue.renderOptions && app.vue.renderOptions.template) {
-    app.vue.resource.template = app.vue.renderOptions.template;
-  }
+  app.view.use('vue', require('./lib/view'));
 };
