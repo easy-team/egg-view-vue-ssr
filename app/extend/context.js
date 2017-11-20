f0b996e74729f3f77fceb4a822e213ed11cfa73e
jacksky007 <jacksky007@qq.com>
2017-11-20 16:59:02 +0800

    fix: set runInNewContext to once

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Cache = require('node-cache');
+const merge = require('lodash.merge')
 const path = require('path');
 
 let cache = null;
@@ -41,11 +42,15 @@ module.exports = {
    * @return {Promise} set body content when fulfilled
    */
   vueRender(name, locals, options = {}) {
+    merge(options, {
+      renderOptions: {
+        runInNewContext: 'once',
+      },
+    })
+
     locals = Object.assign({}, this.app.locals, this.locals, locals);
+
     const config = this.app.config.vuessr;
-    const template = options.renderOptions && options.renderOptions.template || this.app.vue.resource.template;
-    const context = { state: locals };
-    const filepath = path.join(this.app.config.view.root[0], name);
 
     let promise = null;
     let cacheKey = '';
@@ -58,9 +63,12 @@ module.exports = {
     }
 
     if (!promise) {
+      const context = { state: locals };
+      const template = options.renderOptions.template || this.app.vue.resource.template;
       if (options.renderClient) {
         promise = this.renderString(template, context.state);
       } else {
+        const filepath = path.join(this.app.config.view.root[0], name);
         promise = this.app.vue.renderBundle(filepath, context, options).catch(err => {
           if (config.fallbackToClient) {
             this.app.logger.error('[%s] server render bundle error, try client render, the server render error', name, err);
