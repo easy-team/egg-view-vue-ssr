406316a46590076d531f526ea80017aed40b4f2f
hubcarl <hubcarl@126.com>
2018-07-20 19:09:51 +0800

    fix: render client locals merge

--- app/extend/context.js
+++ app/extend/context.js
@@ -4,7 +4,7 @@ module.exports = {
     return this.renderVueClient(name, locals, options);
   },
   renderVueClient(name, locals, options = {}) {
-    locals = this.app.vue.normalizeLocals(this, locals);
+    locals = this.app.vue.normalizeLocals(this, locals, options, false);
     return this.app.vue.renderClient(name, locals, options).then(html => {
       this.body = html;
     });
