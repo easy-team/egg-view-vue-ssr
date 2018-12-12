816dc8f1cc483d356b826b9b7539ebfe5779697f
hubcarl <hubcarl@126.com>
2018-12-12 20:52:21 +0800

    feat: support asset

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,12 +1,21 @@
 'use strict';
 module.exports = {
   renderClient(name, locals, options) {
-    return this.renderVueClient(name, locals, options);
-  },
-  renderVueClient(name, locals, options = {}) {
-    locals = this.app.vue.normalizeLocals(this, locals, options, false);
-    return this.app.vue.renderClient(name, locals, options).then(html => {
+    return this.renderVueClient(name, locals, options).then(html => {
       this.body = html;
     });
   },
+  renderAsset(name, locals, options) {
+    return this.renderVueAsset(name, locals, options).then(html => {
+      this.body = html;
+    });
+  },
+  renderVueAsset(name, locals, options) {
+    return this.app.vue.renderAsset(this, name, locals, options);
+  },
+  renderVueClient(name, locals, options = {}) {
+    locals = this.app.vue.normalizeLocals(this, locals, options, false);
+    return this.app.vue.renderClient(name, locals, options);
+  },
 };
+
